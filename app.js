59ef8ce3bc935cef8aec7a3477a620e6f7fac487
Rachel White <loveless@gmail.com>
2015-11-11 21:13:23 -0500

    set up incremental feeding schedule utilizing cron jobs

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var http = require('http'),
     fs = require('fs'),
     index = fs.readFileSync(__dirname + '/index.html'),
     five = require ("johnny-five"),
-    Particle = require("particle-io");
+    Particle = require("particle-io"),
+    CronJob = require('cron').CronJob;
+
 
 // Set up the access credentials for Particle
-var token = process.env.PARTICLE_KEY || 'your particle key here'; 
-var deviceId = process.env.PHOTON_ID || 'your photon id here';
+var token = process.env.PARTICLE_KEY || 'f2d4ea74c99d6c1706ce78808af4be4cdd5317d9'; 
+var deviceId = process.env.PHOTON_ID || '300023000247343339373536';
 
 // Send index.html to all requests
 var app = http.createServer(function(req, res) {
@@ -27,16 +29,47 @@ var board = new five.Board({
   }) 
 });
 
-// Checking for incoming socket 'click' actions and toggling the LED.
+function makeCronString(input){
+  var accum = '0';
+  var increment = parseInt(input, 10);
+  while( increment < 24 ){
+    accum = accum + ',' + increment
+    increment = increment + parseInt(input, 10);
+  }
+  return '0 0 ' + accum + ' * * *';
+}
+
+var currentJob;
+
 board.on("ready", function() {
-  led = new five.Led('d7');
-  console.log('board is ready');
+
+  var servo = new five.Servo({
+    pin: 'd0', 
+    type: "continuous"
+  });
 
   io.sockets.on('connection', function (socket) {
+
     console.log('sockets on connection');
+
     socket.on('click', function () {
       console.log('socket is clicked');
-      led.toggle();
+      servo.sweep([45, 135]);
+    });
+
+     socket.on('feeding', function(timeValue){
+      if (currentJob) {
+        currentJob.stop();
+      }
+
+      var feedingInterval = makeCronString(timeValue);
+
+      currentJob = new CronJob(feedingInterval, function() {
+        console.log('You will see this message cron job');
+      }, null, true, 'America/New_York');
+
+      console.log(timeValue);
+
     });
   }); 
 });
